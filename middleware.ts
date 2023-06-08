f2fed9b91cd65cf56ece623227c2a5d47219b335
guillermouzner <guillermouz16@gmail.com>
2023-06-08 08:54:19 -0300

    x

--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,31 @@
+// import {Ratelimit} from "@upstash/ratelimit";
+// import {Redis} from "@upstash/redis";
 import {getToken} from "next-auth/jwt";
 import {withAuth} from "next-auth/middleware";
 import {NextResponse} from "next/server";
 
+// const redis = new Redis({
+//   url: process.env.REDIS_URL,
+//   token: process.env.REDIS_SECRET,
+// });
+
+// const ratelimit = new Ratelimit({
+//   redis,
+//   limiter: Ratelimit.slidingWindow(5, "1 h"),
+// });
+
 export default withAuth(
   async function middleware(req) {
+    // const pathname = req.nextUrl.pathname;
+
+    // if (pathname.startsWith("/api")) {
+    //   const ip = req.ip ?? "127.0.0.1";
+
+    //   try {
+    //     const {succes} = await ratelimit.limit(ip);
+    //   } catch (error) {}
+    // }
+
     const token = await getToken({req}); // obtiene el token del request
     const isAuth = !!token; // devuelve true si existe y false si no existe o es invalido
     const isAuthPage = req.nextUrl.pathname.startsWith("/login");
